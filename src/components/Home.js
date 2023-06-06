7cdee1fcb42c866f6479012ec05e27e6abe16b73
Michael Dye <hello@michaeldye.dev>
2023-06-06 14:56:28 -0500

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+const Home = () => {
+  return (
+    <section id="home">
+      <div className="container">
+        <div className="roww">
+          {/* Main Title */}
+          <div className="resumo_fn_main_title">
+            <h3 className="subtitle">Introduction</h3>
+            <h3 className="title">Frontend Engineer</h3>
+            <p className="desc">
+              I am a growth-driven developer dedicated to building a better web.
+            </p>
+            {/* <img src="img/signature.png" alt="image" /> */}
+          </div>
+          {/* /Main Title */}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
